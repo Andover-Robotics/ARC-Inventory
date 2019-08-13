d92d8d54218921c573863e58c29140f633137603
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2019-08-13 14:13:12 -0400

    Setup basics of login authentication, finish login page, fix gradle structure

--- web/src/main/java/com/andoverrobotics/inventory/web/WebApplication.java
+++ web/src/main/java/com/andoverrobotics/inventory/web/WebApplication.java
@@ -1,13 +1,26 @@
 package com.andoverrobotics.inventory.web;
 
+import com.andoverrobotics.inventory.Foundation;
+import com.andoverrobotics.inventory.json.JsonPersistence;
+import com.andoverrobotics.inventory.security.GoogleAccountVerifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class WebApplication {
+    public static Foundation foundation;
 
     public static void main(String[] args) {
-
+        try {
+            JsonPersistence jsonPersistence = new JsonPersistence("data.json");
+            GoogleAccountVerifier googleAccountVerifier = new GoogleAccountVerifier();
+            foundation = new Foundation(jsonPersistence, googleAccountVerifier);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
         SpringApplication.run(WebApplication.class, args);
     }
 
--- web/src/main/resources/static/js/googleSignIn.js
+++ web/src/main/resources/static/js/googleSignIn.js
@@ -1,23 +1,39 @@
 function googleSignInSuccess(googleUser) {
-    // Extract and POST the id_token
-    let id_token = googleUser.getAuthResponse().id_token;
+    // Extract and POST the token
+    let token = googleUser.getAuthResponse().id_token;
     $.ajax({
         type: "POST",
         url: "/login",
         data: JSON.stringify({
-            'token':id_token
+            'token': token
         }),
-        contentType : "application/json",
-        dataType : 'json',
-        success: (data, textStatus, jqXHR) => {
-            console.log(data, textStatus, jqXHR);
+        contentType: "application/json",
+        dataType: 'text',
+        success: () => {
+            let redirectLocation = findGetParameter('redirect');
+
+            if (redirectLocation != null) {
+                window.location.href = redirectLocation;
+            } else
+                window.location.href = '/';
+        },
+        error: (jqXHR) => {
+            signOut(null);
+
+            if (jqXHR.status === 401)
+                $('#err-msg').html('Sorry, but that Google account is not authorized to use ARC Inventory! Please speak with a board member to ensure you are on the whitelist.');
+            else
+                $('#err-msg').html('Sorry, but an unknown error has occurred! Please try again at another time or with a different Google account.<br>If the problem persists, please speak with a CSO.');
         }
     });
 }
 
-function signOut() {
+function signOut(redirect) {
     let auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(() => {
-        console.log('User signed out.');
+        console.log('User signed out');
+        if (typeof redirect === 'string' || redirect instanceof String) {
+            window.location.href = redirect;
+        }
     });
 }
